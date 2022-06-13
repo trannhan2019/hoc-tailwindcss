141191e1797d11fd3a87f1dd7e7ad0ee81ee2893
trannhan2019 <53633928+trannhan2019@users.noreply.github.com>
2022-06-13 17:17:02 +0700

    first commit

---
+++ tailwind-ui/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Disclosure from "./components/Disclosure.vue";
+</script>
+<template>
+  <div>
+    <Disclosure />
+  </div>
+</template>
+
+<style>
+@import "./assets/index.css";
+</style>
---
+++ tailwind-ui/src/components/Disclosure.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+import { ChevronUpIcon } from "@heroicons/vue/solid";
+</script>
+
+<template>
+  <div class="w-full px-4 pt-16">
+    <div class="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
+      <Disclosure v-slot="{ open }">
+        <DisclosureButton
+          class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+        >
+          <span>What is your refund policy?</span>
+          <ChevronUpIcon
+            class="h-5 w-5 text-purple-500"
+            :class="open ? 'transform rotate-180' : ''"
+          />
+        </DisclosureButton>
+        <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
+          If you're unhappy with your purchase for any reason, email us within
+          90 days and we'll refund you in full, no questions asked.
+        </DisclosurePanel>
+      </Disclosure>
+    </div>
+  </div>
+</template>
